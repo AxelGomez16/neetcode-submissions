4c18441b7e12924857ce3b4bb93a5118cd3c8ee9
Axeel👾 <42386036+AxelGomez16@users.noreply.github.com>
2026-06-14 22:39:19 -0600

    Add: max-consecutive-ones - submission-1

---
+++ Data Structures & Algorithms/max-consecutive-ones/submission-1.cpp
@@ -0,0 +1,33 @@
+
+class Solution
+{
+    public:
+        unsigned int ONE  = 1U;
+        unsigned int ZERO = 0U;
+
+        int findMaxConsecutiveOnes(vector<int>& nums)
+        {
+            unsigned int streak_1   = 0U;
+            unsigned int streak_max = 0U;
+            for (const auto& num : nums)
+            {
+                if (num == ONE)
+                {
+                    streak_1 += 1;
+                }
+                else
+                {
+                    if (streak_1 > streak_max)
+                    {
+                        streak_max = streak_1;
+                        streak_1 = ZERO;
+                    }
+                }
+            }
+            if (streak_1 > streak_max)
+            {
+                streak_max = streak_1;
+            }
+            return static_cast<int>(streak_max);
+        }
+};
