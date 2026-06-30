c345373a67801757f6ca6ae43a4ab2ce3a374597
Axeel👾 <42386036+AxelGomez16@users.noreply.github.com>
2026-06-29 21:15:23 -0600

    Add: validate-parentheses - submission-12

---
+++ Data Structures & Algorithms/validate-parentheses/submission-12.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+	bool isValid(string s)
+	{
+		char bracket_t = '\0';
+		vector<char> bracketsNames;
+		bool validBracket = true;
+
+		for (char& letter : s)
+		{
+			if (letter == '(' || letter == '[' || letter == '{')
+			{
+				bracketsNames.push_back(letter);
+			}
+			else
+			{
+				if (bracketsNames.empty())
+				{
+					validBracket = false;
+					break;
+				}
+
+				bracket_t = bracketsNames.back();
+
+				if ( (letter == ')' && bracket_t == '(') || 
+						 (letter == ']' && bracket_t == '[') || 
+						 (letter == '}' && bracket_t == '{') )
+				{
+					bracketsNames.pop_back();
+				}
+				else
+				{
+					validBracket = false;
+					break;
+				}
+			}
+		}
+
+		if (!bracketsNames.empty()) validBracket = false;
+
+		return validBracket;
+
+    }
+};
