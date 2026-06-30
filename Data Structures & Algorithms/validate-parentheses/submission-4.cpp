a474fe97a3d0be02b81e68ce27a5a95e34264436
Axeel👾 <42386036+AxelGomez16@users.noreply.github.com>
2026-06-29 20:18:44 -0600

    Add: validate-parentheses - submission-4

---
+++ Data Structures & Algorithms/validate-parentheses/submission-4.cpp
@@ -0,0 +1,69 @@
+class Solution {
+public:
+	bool isValid(string s)
+	{
+		vector<string> bracketsNames;
+		string bracket_t;
+		string ParenthesesName = "parentheses";
+		string BracketsName = "brackets";
+		string BracesName = "braces";
+		bool validBracket = false;
+		bool atLeastOneElement = false;
+
+		for (char& letter : s)
+		{
+			if (letter == '(')
+			{
+				bracketsNames.push_back(ParenthesesName);
+				atLeastOneElement = true;
+			}
+			else if (letter == '[')
+			{
+				bracketsNames.push_back(BracketsName);
+				atLeastOneElement = true;
+			}
+			else if (letter == '{')
+			{
+				bracketsNames.push_back(BracesName);
+				atLeastOneElement = true;
+			}
+			else if (letter == ')' && bracketsNames.size() != 0)
+			{
+				bracket_t = bracketsNames.back();
+				if (ParenthesesName == bracket_t)
+				{
+					bracketsNames.pop_back();
+				}
+			}
+			else if (letter == ']' && bracketsNames.size() != 0)
+			{
+				bracket_t = bracketsNames.back();
+				if (BracketsName == bracket_t)
+				{
+					bracketsNames.pop_back();
+				}
+			}
+			else if (letter == '}' && bracketsNames.size() != 0)
+			{
+				bracket_t = bracketsNames.back();
+				if (BracesName == bracket_t)
+				{
+					bracketsNames.pop_back();
+				}
+			}
+			else
+			{
+				atLeastOneElement = false;
+				validBracket = false;
+				break;
+			}
+		}
+
+		if (bracketsNames.size() == 0 && atLeastOneElement == true)
+		{
+			validBracket = true;
+		}
+
+		return validBracket;
+    }
+};
